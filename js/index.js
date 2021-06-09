ddd402ecfbc12f6d60aaa81a56bbf0e9545c0be8
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2021-06-09 19:09:42 +0100

    Add hero section to index.html, refactor
    Move some CSS from main.css to index.css
    
    Moved contact link's color to a variable called primary-color
    
    Dimmed header's box-shadow
    
    Added a list of texts that can appear in the hero section

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const texts = [
+	"A Level Biology Tutoring",
+	"A Level Chemistry Tutoring",
+	"A Level Maths Tutoring",
+	"A Level Computer Science Tutoring",
+	"GCSE Tutoring",
+	"11+ Tutoring",
+	"Medicine University Application Advice",
+	"Medicine Personal Statment Advice",
+	"Programming Tutoring",
+	"Programming Courses",
+	"Programming Projects"
+]
+const waitPerCharacterDuration = 175;
+const typeDuration = 60;
+const textContainer = document.querySelector(".textContainer");
+
+async function typeText() {
+	let oldText = textContainer.innerText;
+	let textsToChooseFrom = texts.filter((val) => (val != oldText));
+	let newText = textsToChooseFrom[Math.floor(Math.random() * textsToChooseFrom.length)];
+	for (let i = oldText.length; i >= 0; i--) {
+		textContainer.innerHTML = oldText.substring(0, i) + '<span class="cursor"></span>';
+		await new Promise(resolve => setTimeout(resolve, typeDuration));
+	};
+	for (let i = 0; i <= newText.length; i++) {
+		textContainer.innerHTML = newText.substring(0, i) + '<span class="cursor"></span>';
+		await new Promise(resolve => setTimeout(resolve, typeDuration));
+	};
+	setTimeout(typeText, newText.length * waitPerCharacterDuration);
+}
+typeText();

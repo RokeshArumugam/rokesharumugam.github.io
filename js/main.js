44e5fdbc3b59ee4b3b9f57ab0cd7dea3b331e823
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2021-07-09 17:45:55 +0100

    Dynamically load header and footer
    Header and footer inner html are stored in files in components folder in
    the root directory

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 const urlParams = new URLSearchParams(window.location.search);
+
+async function loadHeaderAndFooter() {
+	fetch("/components/header.html")
+		.then(response => {
+			return response.text()
+		})
+		.then(data => {
+			document.getElementsByTagName("header")[0].innerHTML = data;
+		});
+
+	fetch("/components/footer.html")
+		.then(response => {
+			return response.text()
+		})
+		.then(data => {
+			document.getElementsByTagName("footer")[0].innerHTML = data;
+		});
+};
+
+loadHeaderAndFooter();

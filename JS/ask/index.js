97962e38cc0bfe4f48400541a0e0894e861d3fba
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2021-09-05 21:16:10 +0100

    Add Gabriella's testimonial

--- JS/ask/index.js
+++ JS/ask/index.js
@@ -3,6 +3,11 @@ const testimonials = [
 		"authorName": "Rishal Narayan",
 		"authorPosition": "A Level Student",
 		"quoteText": "The help Rokesh gave me with my personal statement was invaluable and extremely useful. Going through each paragraph and planning it together gave me a much better understanding of what a good personal statement consists of."
+	},
+	{
+		"authorName": "Gabriella Kwei",
+		"authorPosition": "Parent of A Level Student",
+		"quoteText": "Rokesh was extremely helpful and personable - we found his advice to be informative and practical. The session exceeded expectations as it enabled my daughter to get tailored feedback. I would have been happy to pay £50 for Rokesh's advice and would highly recommend him to prospective students."
 	}
 ];
 const testimonialTemplate = document.getElementById("testimonialTemplate");

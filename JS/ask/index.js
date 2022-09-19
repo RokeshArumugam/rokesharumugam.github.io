be4398749fef59c6d4f8f4302939db5e31e6b798
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2022-09-19 18:55:06 +0100

    Add Jaysri's testimonial

--- JS/ask/index.js
+++ JS/ask/index.js
@@ -13,6 +13,11 @@ const testimonials = [
 		"authorName": "Samyuktha Ramesh Dhayanand",
 		"authorPosition": "A Level Student",
 		"quoteText": "I am very appreciative of Rokesh's assistance with my personal statement because it was incredibly helpful. I valued how he carefully went over each paragraph and advised me on the necessary improvements and editing to make it the best possible final draft. It significantly improved my personal statement, in my opinion. Thank you so much."
+	},
+	{
+		"authorName": "Jaysri Govindarajan",
+		"authorPosition": "A Level Student",
+		"quoteText": "Rokesh has been extremely helpful with my medicine application! He did not hold back on giving me invaluable advice. He has spent hours helping me create a plan for my personal statement, from outlining the qualities of a doctor to finding examples of these qualities in my own experiences. Thank you very much for all the help Rokesh!"
 	}
 ];
 

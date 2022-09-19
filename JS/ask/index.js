91c1a270ee12611ed55f46fd5374ab945a2f20d1
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2022-09-19 16:23:09 +0100

    Add Samyuktha's testimonial

--- JS/ask/index.js
+++ JS/ask/index.js
@@ -8,6 +8,11 @@ const testimonials = [
 		"authorName": "Gabriella Kwei",
 		"authorPosition": "Parent of A Level Student",
 		"quoteText": "Rokesh was extremely helpful and personable - we found his advice to be informative and practical. The session exceeded expectations as it enabled my daughter to get tailored feedback. I would have been happy to pay £50 for Rokesh's advice and would highly recommend him to prospective students."
+	},
+	{
+		"authorName": "Samyuktha Ramesh Dhayanand",
+		"authorPosition": "A Level Student",
+		"quoteText": "I am very appreciative of Rokesh's assistance with my personal statement because it was incredibly helpful. I valued how he carefully went over each paragraph and advised me on the necessary improvements and editing to make it the best possible final draft. It significantly improved my personal statement, in my opinion. Thank you so much."
 	}
 ];
 

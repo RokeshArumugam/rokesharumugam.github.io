dd1e3c94b1bbb744d9b71acd1595d37af58d1a85
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2023-08-18 11:07:30 +0100

    Update character sheet text, add Prandium listing

--- JS/projects/index.js
+++ JS/projects/index.js
@@ -23,8 +23,8 @@ const projects = [
 		"startDate": "2022-09",
 		"description": [
 			"Character Sheet is a digital character sheet that has some web-scraping-powered automations.",
-			"I use this instead of my paper character sheet when I'm playing D&D. I just copy the URL when I'm done and then I can use it later to access the same character sheet again.",
-			"All of the data is saved in the URL so anyone can use this and save data without an account."
+			"I use this instead of my paper character sheet when I'm playing D&D. I just close the tab when I'm done and reopen it next time to access my previous character sheets.",
+			"All of the data is saved in the browser on your device so anyone can use this and save data without an account. Alternatively you can also download your character sheet as a file to use on a different device."
 		]
 	},
 	{
@@ -44,15 +44,15 @@ const projects = [
 			"I add the URLs of products I would like to monitor to a file and my phone is pinged when the price changes. It helps me save a few pounds every so often."
 		]
 	},
-	// {
-	// 	"name": "Prandium",
-	// 	"startDate": "2021-11",
-	// 	"description": [
-	// 		"Prandium is a bot to book meals at Gonville and Caius college, University of Cambridge.",
-	// 		"We have to book meals in advance and they can be very competitive. People wake up early to book meals and still often miss them.",
-	// 		"So I created Prandium. I have been able to sleep in and get a place at any meal I would like for over a year now. I also let some friends occasionally use it too."
-	// 	]
-	// },
+	{
+		"name": "Prandium",
+		"startDate": "2021-11",
+		"description": [
+			"Prandium is a bot to book meals at Gonville and Caius college, University of Cambridge.",
+			"We have to book meals in advance and they can be very competitive. People wake up early to book meals and still often miss them.",
+			"So I created Prandium. I have been able to sleep in and get a place at any meal I would like for over a year now. I also let some friends occasionally use it too."
+		]
+	},
 	{
 		"name": "Invoice Generator",
 		"startDate": "2021-06",
